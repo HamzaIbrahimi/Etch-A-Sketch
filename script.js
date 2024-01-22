8c6bdd8a16de93a844f11e3e6338a1241213bb21
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-01-22 19:25:16 +0100

    Adding the grid to an arrow function

--- script.js
+++ script.js
@@ -3,10 +3,13 @@
 const main_container = document.querySelector('#main_container');
 const grid = document.querySelector('#the_grid')
 
-
-for(let i=0; i<= 800; i++) {
-    const divs = document.createElement('div');
-    divs.className = 'squares';
-    divs.style.cssText = 'width: 30px; height: 30px; border: 1px solid black; margin: 0; padding:0; flex: auto;';
+const createGrid16x16 = () => {
+  for (let i = 0; i < 256; i++) {
+    const divs = document.createElement("div");
+    divs.className = "squares";
+    divs.style.cssText =
+      " width: 40px; height:40px; border: 1px solid black; margin: 0; padding:0; flex: auto;";
     grid.appendChild(divs);
+  }
 }
+createGrid16x16();
