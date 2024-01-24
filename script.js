3f76ab00e6bfc27ab04696d369b9008a6ac57aa9
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-01-24 17:51:22 +0100

    new function with grid cells formula

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 //Create global variables for DOM manipulation
 
-const main_container = document.querySelector('#main_container');
-const grid = document.querySelector('#the_grid')
+const grid = document.querySelector('#grid');
+const changeGrid = document.querySelector('#change_grid');
+const black = document.querySelector('#draw_black');
+const erase = document.querySelector('#erase');
+const clear = document.querySelector('#clear');
+const rainbow = document.querySelector('#rainbow');
+let defaultGrid = 16;
 
-const createGrid16x16 = () => {
-  for (let i = 0; i < 256; i++) {
-    const divs = document.createElement("div");
-    divs.className = "squares";
-    divs.style.cssText =
-      " width: 40px; height:40px; border: 1px solid black; margin: 0; padding:0; flex: auto;";
-    grid.appendChild(divs);
+
+
+function createGrid (size) {
+  for(let i = 0; i<size*size; i++) {
+    const gridCells = document.createElement('div');
+    gridCells.className = 'cells';
+    let cellWidth = grid.clientWidth/size + 'px';
+    let cellHeight = grid.clientHeight/size + 'px';
+    gridCells.style.width = `${cellWidth}`;
+    gridCells.style.height = `${cellHeight}`;
+    gridCells.style.border = '1px solid black'
+    grid.appendChild(gridCells);
+    gridCells.addEventListener('mouseover', () => {
+      gridCells.style.background = 'black';
+    })
+    
   }
 }
-createGrid16x16();
+
+createGrid(16);
+
