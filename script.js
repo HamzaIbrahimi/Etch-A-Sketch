f8336cfa89b817844b1b3ec8267c66923c4d2751
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-06-17 23:35:19 +0200

    cleaning code by creating function then add to event listener

--- script.js
+++ script.js
@@ -10,51 +10,49 @@ let defaultGrid = 16;
 let cellsArray = []; //Used to manipulate dom through pushing grid cells to it
 let gridWidth = grid.clientWidth;
 let gridHeight = grid.clientHeight;
-console.log(gridWidth);
-console.log(gridHeight);
 
-
-function randomColor () { //function the returns a random hex color
-  let colors = '0123456789ABCDEF';
-  let hex = '#';
-  for(let i=0; i<6; i++) {
-   hex += colors[Math.floor(Math.random()*colors.length)];
+function randomColor() {
+  //function the returns a random hex color
+  let colors = "0123456789ABCDEF";
+  let hex = "#";
+  for (let i = 0; i < 6; i++) {
+    hex += colors[Math.floor(Math.random() * colors.length)];
   }
   return hex;
- }
-
-
- 
-function createGrid(size) { //Function to create a grid 
-  //Instead of creating all 256 divs in one for loop create a nested for loop
-  for(let i = 0; i<size; i++ ) {//columns
-      let div_column = document.createElement('div');
-      grid.appendChild(div_column);
-      cellsArray.push(div_column);
-      div_column.style.width = (gridWidth/size) + 'px';
-
-        for(let j = 0; j<size; j++) {//rows
-          let div_row = document.createElement('div');
-          grid.appendChild(div_row);
-          div_row.style.width = (gridWidth/size) + 'px';
-          cellsArray.push(div_row);
-        }
-  }
-
-  
-
- 
 }
 
-function mainGrid() {//main 16x16 default display 
+function createGrid(size) {
+  //Function to create a grid
+  //Instead of creating all 256 divs in one for loop create a nested for loop
+  for (let i = 0; i < size; i++) {
+    //columns
+    let div_column = document.createElement("div");
+    grid.appendChild(div_column);
+    cellsArray.push(div_column);
+    div_column.style.width = gridWidth / size + "px";
+
+    for (let j = 0; j < size; j++) {
+      //rows
+      let div_row = document.createElement("div");
+      grid.appendChild(div_row);
+      div_row.style.width = gridWidth / size + "px";
+      cellsArray.push(div_row);
+    }
+  }
+}
+
+function mainGrid() {
+  //main 16x16 default display
   createGrid(defaultGrid);
 }
 mainGrid();
 
-
-function changeGridSize() { //Function & EventListener to change grid size
+function changeGridSize() {
+  //Function & EventListener to change grid size
   do {
-    size = Math.floor( parseInt(prompt("Choose Grid Size: Whole Number between 16 & 40")));
+    size = Math.floor(
+      parseInt(prompt("Choose Grid Size: Whole Number between 16 & 100"))
+    );
   } while (!Number.isInteger(size));
   grid.textContent = "";
   createGrid(size);
@@ -62,55 +60,42 @@ function changeGridSize() { //Function & EventListener to change grid size
 
 changeGrid.addEventListener("click", changeGridSize);
 
+//Function to change background color of each cell on mouse over
+function changeBackgroundToBlack() {
+  for (const cell of cellsArray) {
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = "black";
+    });
+  }
+}
 
-//clearing the sketch area testing event delegation
-clear.addEventListener('click', () => {
+window.addEventListener('DOMContentLoaded', changeBackgroundToBlack)
+
+//function to reverse background color changes to none (acting as an erase)
+function eraseGrid() {
   cellsArray.forEach((cell) => {
-    cell.style.background = '';
-  })
+    cell.addEventListener("mouseover", () => {
+      cell.style.background = "";
+    });
+  });
+}
+
+//function to change background color to random color
+function rainbowStyle() {
+  for (const cell of cellsArray) {
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = randomColor();
+    });
+  }
+}
+
+//Event listeners on the buttons
+black.addEventListener("click", changeBackgroundToBlack);
+erase.addEventListener("click", eraseGrid);
+rainbow.addEventListener("click", rainbowStyle);
+clear.addEventListener("click", () => {
+  cellsArray.forEach((cell) => {
+    cell.style.background = "";
+  });
 });
 
-
-
-//old code 
-// if (size >= 16 && size <= 40) {
-//   for (let i = 0; i < size * size; i++) {
-//     const gridCells = document.createElement("div");
-//     gridCells.className = "cells";
-//     let cellWidth = grid.clientWidth / size + "px";
-//     let cellHeight = grid.clientHeight / size + "px";
-//     gridCells.style.cssText = `border: 1px solid black;
-//       width: ${cellWidth}; height: ${cellHeight};`;
-//     grid.appendChild(gridCells);
-//     cellsArray.push(gridCells);
-//         //event listener to draw black
-//         gridCells.addEventListener('mouseover', () => {
-//           gridCells.style.background = 'black'
-//         })
-//     //event listener to erase 
-//         erase.addEventListener('click', () => {
-//           gridCells.addEventListener('mouseover', () => {
-//             gridCells.style.background ='';
-//           })
-//         })
-//         //event listener to toggle back to black 
-//         black.addEventListener('click', () => {
-//           gridCells.addEventListener('mouseover', () => {
-//             gridCells.style.background ='black';
-//           })
-//         })
-
-//         //event listener for colorful mode
-//         rainbow.addEventListener('click', () => {
-//           gridCells.addEventListener('mouseover', () => {
-//             gridCells.style.background = randomColor();
-//           })
-//         })
-       
-//   }
-
-// } else {
-//   alert('You did not pick a number between 16 & 40--Grid Now Default');
-//   mainGrid();
-  
-// }
